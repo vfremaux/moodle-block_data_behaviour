706426c1530b72bfcf2c4cd115cf24717dfdd743
Valery Fremaux <valery.fremaux@gmail.com>
2024-05-02 11:15:07 +0200

    Automated sync from SVN

---
+++ edit_form.php
@@ -0,0 +1,55 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    block_quiz_behaviour
+ * @category   blocks
+ * @copyright  2018 Valery Fremaux (valery.fremaux@gmail.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die();
+
+class block_data_behaviour_edit_form extends block_edit_form {
+
+    protected function specific_definition($mform) {
+        global $DB, $COURSE;
+
+        $mform->addElement('header', 'configheader', get_string('databehaviourconfig', 'block_data_behaviour'));
+
+        $datas = $DB->get_records('data', array('course' => $COURSE->id));
+
+        foreach ($datas as $d) {
+
+            $dname = format_string($d->name);
+
+            $mform->addElement('static', 'config_dname'.$d->id, $dname);
+
+            $mform->addElement('advcheckbox', 'config_seeonlymydata'.$d->id, get_string('seeonlymydata', 'block_data_behaviour'));
+
+            $mform->addElement('advcheckbox', 'config_notabs'.$d->id, get_string('notabs', 'block_data_behaviour'));
+
+            $mform->addElement('advcheckbox', 'config_nosearch'.$d->id, get_string('nosearch', 'block_data_behaviour'));
+
+            $mform->addElement('advcheckbox', 'config_nosingle'.$d->id, get_string('nosingle', 'block_data_behaviour'));
+
+            $mform->addElement('advcheckbox', 'config_noadmin'.$d->id, get_string('noadmin', 'block_data_behaviour'));
+
+            $mform->addElement('text', 'config_behaviourtags'.$d->id, get_string('tags', 'block_data_behaviour'));
+            $mform->setType('config_behaviourtags'.$d->id, PARAM_TEXT);
+
+        }
+    }
+}
---
+++ lang/fr/block_data_behaviour.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Block Quiz Behaviour language strings.
+ *
+ * @package    block_data_behaviour
+ * @copyright  2018 Valery Fremaux
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['data_behaviour:addinstance'] = 'Add a Data Module Behaviour block';
+
+$string['help'] = 'Editez ce bloc pour activer les changements de comportement non standard de l\'activité Base de données dans le cours.';
+$string['databehaviourconfig'] = 'Comportement des bases de données';
+$string['pluginname'] = 'Comportement des bases de données.';
+$string['seeonlymydata'] = 'Voir seulement mes fiches';
+$string['nosearch'] = 'Masquer la recherche de fiches';
+$string['nosingle'] = 'Masquer l\'affichage individuel de fiche';
+$string['notabs'] = 'Simplifier les onglets';
+$string['noadmin'] = 'Masquer l\'administration';
+$string['tags'] = 'Tags comportementaux';
+$string['youhave'] = '<p> Vous avez {$a} bases de données dans ce cours</p>';
